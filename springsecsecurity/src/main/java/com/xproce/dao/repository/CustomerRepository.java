3f04ddf73529c0721a3794705b54644abda66907
salma <salma@gmail.com>
2024-01-20 21:18:41 +0100

    first commit

---
+++ springsecsecurity/src/main/java/com/xproce/dao/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.xproce.dao.repository;
+
+import com.xproce.dao.entites.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    List<Customer> findByEmail(String email);
+}
